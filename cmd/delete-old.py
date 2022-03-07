fa674fa47bb575e94265dffea96640cfd9d80343
Mike Powell <mike@lakeslegendaries.com>
2022-03-07 09:34:00 -0500

    reading GIT_TOKEN from env var

--- cmd/delete-old.py
+++ cmd/delete-old.py
@@ -1,13 +1,26 @@
 """Delete old, untagged images on GHCR"""
 
 import json
+import os
+from os.path import isfile
 from subprocess import run
 
 
 if __name__ == '__main__':
 
+    # get git token
+    if 'GIT_TOKEN' in os.environ:
+        git_token = os.environ['GIT_TOKEN']
+    elif isfile('.dockerkey'):
+        git_token = open('.dockerkey').read().strip()
+    else:
+        raise PermissionError(
+            'No access to GHCR. '
+            'You must provide a git token, either as a '
+            '.dockerkey file, or as the env var GIT_TOKEN.'
+        )
+
     # get current versions
-    git_token = open('.dockerkey').read().strip()
     response = json.loads(run(
         f"""
             curl -u lakes-legendaries:{git_token}
@@ -17,6 +30,8 @@ if __name__ == '__main__':
         check=True,
         text=True,
     ).stdout)
+
+    # delete untagged versions
     for version in response:
         if not version['metadata']['container']['tags']:
             id = version['id']
