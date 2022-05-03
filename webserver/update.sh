1514242ce2318872e5bebe2e6df5cd6aabf27ba8
Mike Powell <mike@lakeslegendaries.com>
2022-05-03 10:29:51 -0400

    fixing install requirements command

--- webserver/update.sh
+++ webserver/update.sh
@@ -17,7 +17,7 @@ function new_venv () {
     python3.9 -m venv .venv
     source .venv/bin/activate
     python -m pip install --upgrade pip
-    python -m pip -r requirements.txt
+    python -m pip install -r requirements.txt
     export PYTHONPATH=$(pwd)
 }
 
