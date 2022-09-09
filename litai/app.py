70533e7db9d1ee3036806484c69bfbd62d987b0b
Mike Powell <mike@lakelegendaries.com>
2022-09-09 17:55:04 +0000

    updating serach part of api

--- litai/app.py
+++ litai/app.py
@@ -1,8 +1,3 @@
-from datetime import datetime
-import os
-from os import remove
-from os.path import isfile, join
-from subprocess import run
 from typing import Optional
 
 from fastapi import FastAPI
@@ -41,7 +36,7 @@ def search(
     min_date: Optional[str] = None,
     scores_table: Optional[str] = None,
 ):
-    articles = SearchEngine('data/pubmed.db').search(
+    articles = SearchEngine().search(
         keywords=keywords.split() if keywords else None,
         max_date=max_date,
         min_date=min_date,
@@ -60,58 +55,58 @@ def search(
     }
 
 
-@app.get("/feedback/{action}")
-def feedback(
-    action: str,
-    pmid: Optional[str] = None,
-    table: Optional[str] = None,
-    token: Optional[str] = None,
-):
+# @app.get("/feedback/{action}")
+# def feedback(
+#     action: str,
+#     pmid: Optional[str] = None,
+#     table: Optional[str] = None,
+#     token: Optional[str] = None,
+# ):
 
-    # write feedback to file
-    fname = datetime.now().isoformat()
-    with open(fname, 'w') as file:
-        print(f'action: {action}', file=file)
-        print(f'pmid: {pmid}', file=file)
-        print(f'table: {table}', file=file)
-        print(f'token: {token}', file=file)
+#     # write feedback to file
+#     fname = datetime.now().isoformat()
+#     with open(fname, 'w') as file:
+#         print(f'action: {action}', file=file)
+#         print(f'pmid: {pmid}', file=file)
+#         print(f'table: {table}', file=file)
+#         print(f'token: {token}', file=file)
 
-    # authenticate with azure
-    az_key = 'AZURE_STORAGE_CONNECTION_STRING'
-    if az_key not in os.environ:
+#     # authenticate with azure
+#     az_key = 'AZURE_STORAGE_CONNECTION_STRING'
+#     if az_key not in os.environ:
 
-        # load in azure authentication token
-        conn_fname = join(os.environ['SECRETS_DIR'], 'litai-fileserver')
-        if not isfile(conn_fname):
-            return {
-                'Status': 'Failed',
-                'Reason': 'Could not authenticate with Azure. '
-                          f'File {conn_fname} DNE',
-            }
+#         # load in azure authentication token
+#         conn_fname = join(os.environ['SECRETS_DIR'], 'litai-fileserver')
+#         if not isfile(conn_fname):
+#             return {
+#                 'Status': 'Failed',
+#                 'Reason': 'Could not authenticate with Azure. '
+#                           f'File {conn_fname} DNE',
+#             }
 
-        # add to env vars
-        os.environ[az_key] = open(conn_fname, 'r').read()
+#         # add to env vars
+#         os.environ[az_key] = open(conn_fname, 'r').read()
 
-    # upload to azure
-    run(
-        [
-            'az',
-            'storage',
-            'blob',
-            'upload',
-            '-f',
-            fname,
-            '-c',
-            'feedback',
-            '-n',
-            fname,
-        ],
-        capture_output=True,
-        check=True,
-    )
+#     # upload to azure
+#     run(
+#         [
+#             'az',
+#             'storage',
+#             'blob',
+#             'upload',
+#             '-f',
+#             fname,
+#             '-c',
+#             'feedback',
+#             '-n',
+#             fname,
+#         ],
+#         capture_output=True,
+#         check=True,
+#     )
 
-    # clean-up
-    remove(fname)
+#     # clean-up
+#     remove(fname)
 
-    # return success
-    return {'Status': 'Succeeded'}
+#     # return success
+#     return {'Status': 'Succeeded'}
